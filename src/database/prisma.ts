e004af38593d57f4dc043ed77bdecfa62878a6a7
tarla hosseinzadeh <tarla.hosseinzadeh@gmail.com>
2026-08-21 00:14:18 +0330

    fix production database connection

--- src/database/prisma.ts
+++ src/database/prisma.ts
@@ -1,7 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 
-
 const globalForPrisma =
   globalThis as unknown as {
     prisma: PrismaClient | undefined;
@@ -9,11 +8,9 @@ const globalForPrisma =
 
 
 function getDatabaseConfig() {
-
   const url = new URL(
     process.env.DATABASE_URL!
   );
-
 
   return {
     host: url.hostname,
@@ -22,21 +19,20 @@ function getDatabaseConfig() {
     password: url.password,
     database: url.pathname.replace("/", ""),
   };
-
 }
 
 
-const adapter =
-  new PrismaMariaDb(
-    getDatabaseConfig()
-  );
+const adapter = new PrismaMariaDb({
+  ...getDatabaseConfig(),
+  connectionLimit: 1,
+});
 
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     adapter,
-  });
+});
 
 
 if (process.env.NODE_ENV !== "production") {
